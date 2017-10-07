3fd96315d1bcf4338a57b4192d58a81cd8638088
John Burchell <john.a.burchell@gmail.com>
2017-10-07 11:50:27 +0100

    Basic networking headers etc added

---
+++ Networking/Networking/socket.h
@@ -0,0 +1,11 @@
+#ifndef _SOCKET_H_
+#define _SOCKET_H_
+#pragma once
+
+namespace Socket
+{
+
+
+} // namespace Socket
+
+#endif // SOCKET_H_
---
+++ Networking/Networking/tcpListener.h
@@ -0,0 +1,7 @@
+#ifndef _TCP_LISTENER_H_
+#define _TCP_LISTENER_H_
+
+#pragma once
+
+
+#endif // TCP_LISTENER_H_
